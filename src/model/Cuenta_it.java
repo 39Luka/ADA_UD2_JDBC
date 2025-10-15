5f5bfba814e9083d8c87604298beeb0b360b233c
silcaclei <silviacachonleiva@gmail.com>
2025-10-15 17:14:11 +0200

    v7

---
+++ src/model/Cuenta_it.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Cuenta_it {
+    int id;
+    int empleado_id;
+    String username;
+    LocalDateTime creado_en;
+}
---
+++ src/model/Empleado.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Empleado {
+    int id;
+    String dni;
+    String nombre;
+    String email;
+    String departamento;
+    LocalDateTime fecha_alta;
+
+    public Empleado(String dni, String nombre, String email, String departamento) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.email = email;
+        this.departamento = departamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public LocalDateTime getFecha_alta() {
+        return fecha_alta;
+    }
+
+    public void setFecha_alta(LocalDateTime fecha_alta) {
+        this.fecha_alta = fecha_alta;
+    }
+}
